a815a9d98a1697ae6eda8da9f79d83f5ff9cc2e9
Lissandro0 <gonzalezlissandro64@gmail.com>
2024-01-11 16:55:19 -0400

    feat: finish task

--- Carrito/CarritoApp/views.py
+++ Carrito/CarritoApp/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
+from CarritoApp.models import Producto
+
+
 def tienda(request):
     #return HttpResponse("Hola Lissandro0")
-    return render(request, "tienda.html")
+    productos = Producto.objects.all()
+    return render(request, "tienda.html", {'productos':productos})
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("CarritoApp:Tienda")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("CarritoApp:Tienda")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("CarritoApp:Tienda")
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("CarritoApp:Tienda")
